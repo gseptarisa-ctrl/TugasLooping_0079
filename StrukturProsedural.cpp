a6d1d54fa1d3b5414983ecaa1195ea1f348e6e85
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-03-31 22:04:43 +0700

    membuat fungsi tipe bool

--- StrukturProsedural.cpp
+++ StrukturProsedural.cpp
@@ -5,3 +5,10 @@ using namespace std;
 //variabel global
 int n;          // menyimpan angka yang diinput
 int pilihan;    // menyimpan pilihan menu
+
+//fungsi cek bilangan prima
+bool cekPrima(int n){
+    if (n <= 1){
+        return false;
+    }
+    
