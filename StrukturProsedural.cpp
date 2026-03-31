cd0901c6d6e3e2821f0d0c411485f4b05640a854
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-03-31 22:50:38 +0700

    menambahkan prosedur menu

--- StrukturProsedural.cpp
+++ StrukturProsedural.cpp
@@ -66,3 +66,13 @@ void hasilFibonacci(){
         cout << n << " bukan bilangan fibonacci." << endl;
     }
 }
+
+//prosedur menu
+void menu(){
+    cout << "\n=== MENU PROGRAM ===" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Masukkan pilihan : ";
+    cin >> pilihan;
+}
